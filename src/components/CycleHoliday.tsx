e20fae7ae3fd51dd8690a093061b3f12b3f9b582
Adam Liu <adamliu@udel.edu>
2023-09-27 23:54:44 -0400

    finished task 8

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,44 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    // Labor Day, Christmas, Halloween, New Years, Thanksgiving
+    // Alphabetical: 🛠️🎄🎃🍾🦃
+    // Time of year: 🍾🛠️🎃🦃🎄
+    const [holiday, setHoliday] = useState<string>("🛠️");
+
+    const alphabetical: Record<string, string> = {
+        "🛠️": "🎄",
+        "🎄": "🎃",
+        "🎃": "🍾",
+        "🍾": "🦃",
+        "🦃": "🛠️"
+    };
+    const chronological: Record<string, string> = {
+        "🍾": "🛠️",
+        "🛠️": "🎃",
+        "🎃": "🦃",
+        "🦃": "🎄",
+        "🎄": "🍾"
+    };
+    return (
+        <div>
+            <p>Holiday: {holiday}</p>
+            <div>
+                <Button
+                    onClick={() =>
+                        setHoliday((prevHoliday) => alphabetical[prevHoliday])
+                    }
+                >
+                    Advance by Alphabet
+                </Button>
+                <Button
+                    onClick={() =>
+                        setHoliday((prevHoliday) => chronological[prevHoliday])
+                    }
+                >
+                    Advance by Year
+                </Button>
+            </div>
+        </div>
+    );
 }
--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,27 @@ export function d6(): number {
 }
 
 export function TwoDice(): JSX.Element {
-    return <div>Two Dice</div>;
+    const [dice1, setDice1] = useState<number>(1);
+    const [dice2, setDice2] = useState<number>(2);
+    const rollDice = (die: number) => {
+        if (die === 1) {
+            setDice1(d6());
+        } else {
+            setDice2(d6());
+        }
+    };
+    return (
+        <div>
+            <p>
+                <span data-testid="dice-1">{dice1}</span>
+                <span data-testid="dice-2">{dice2}</span>
+            </p>
+            <div>
+                <Button onClick={() => rollDice(1)}>Roll Left</Button>
+                <Button onClick={() => rollDice(2)}>Roll Right</Button>
+            </div>
+            {dice1 === dice2 && dice1 === 1 && <p>Lose</p>}
+            {dice1 === dice2 && dice1 !== 1 && <p>Win</p>}
+        </div>
+    );
 }
