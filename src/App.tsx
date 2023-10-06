f3ea9f4c0b4f69e988636a7c150d9d69a3043918
Adam Liu <adamliu@udel.edu>
2023-10-06 15:51:04 -0400

    returned app.tsx to original

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,50 @@ function App(): JSX.Element {
             <header className="App-header">
                 Adam Liu UD CISC275 with React Hooks and TypeScript
             </header>
+            <h1>This is a header</h1>
+            <p>Hello World</p>
+            <img
+                src="../assets/images/doraemon1.jpg"
+                alt="This is an image of doraemon"
+            />
+            <ol>
+                <li>Thing 1</li>
+                <li>Thing 2</li>
+                <li>Thing 3</li>
+            </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <p>
+                Edit <code>src/App.tsx</code> and save. This page will
+                automatically reload.
+            </p>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            First Column
+                            <div
+                                style={{
+                                    width: "50px",
+                                    height: "30px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                        </Col>
+                        <Col>
+                            Second Column
+                            <div
+                                style={{
+                                    width: "50px",
+                                    height: "30px",
+                                    backgroundColor: "red"
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             <hr></hr>
             <CheckAnswer expectedAnswer="42"></CheckAnswer>
             <hr></hr>
